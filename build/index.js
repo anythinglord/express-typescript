a122eb662b65dc1a6533542d5ccc881d76a4ebc0
anythinglord <sjsalcedoa@unal.edu.co>
2024-10-16 09:43:46 -0500

    create diaries service, routes

--- build/index.js
+++ build/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.use(express_1.default.json());
+app.use(express_1.default.json()); // middleware -> transform the req.body to json
 const PORT = 3005;
 app.get('/ping', (_req, res) => {
     console.log('asdfasdf');
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express'
+import diaryRouter from './routes/diaries'
+
 const app = express()
-app.use(express.json())
+app.use(express.json()) // middleware -> transform the req.body to json
 
 const PORT = 3005
 app.get('/ping', (_req, res) => {
-    console.log('asdfasdf')
     res.send('pong')
 })
+
+app.use('/api/diaries', diaryRouter)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
